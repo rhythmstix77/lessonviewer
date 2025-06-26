eed18e40425745d68eeecfa4c31a127f175def97
rhythmstix77 <rob@rhythmstix.co.uk>
2025-06-26 14:48:50 +0100

    Fix Grid3X3 icon import error

--- project/src/components/ActivityLibrary.tsx
+++ project/src/components/ActivityLibrary.tsx
@@ -2,7 +2,7 @@ import React, { useState, useMemo } from 'react';
 import { 
   Search, 
   Filter, 
-  Grid3X3, 
+  Grid, 
   List, 
   Plus, 
   BookOpen, 
@@ -162,7 +162,7 @@ export function ActivityLibrary({ onActivitySelect, selectedActivities, classNam
                 viewMode === 'grid' ? 'bg-white bg-opacity-20' : 'hover:bg-white hover:bg-opacity-10'
               }`}
             >
-              <Grid3X3 className="h-5 w-5" />
+              <Grid className="h-5 w-5" />
             </button>
             <button
               onClick={() => setViewMode('list')}
